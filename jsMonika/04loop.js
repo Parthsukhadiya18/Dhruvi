2731c9545d9b3b4eaa28861eab800badf242de46
Parth <Parthsir18@gmail.com>
2023-10-16 16:00:59 +0530

    first commit

---
+++ jsMonika/04loop.js
@@ -0,0 +1,20 @@
+
+let a = 1;
+
+// while(a<=100){
+//     a++;
+// console.log("Hello Parth");
+// }
+// let b = 1;
+// do {
+    
+//     console.log("Do Hello Parth"+ b);
+//     b++;
+// } while (b<=100);
+
+
+for(i=10;i>=0;i--){
+  console.log("Cold Drink " + i);
+}
+
+
